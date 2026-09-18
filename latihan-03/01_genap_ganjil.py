dd73c475f8c5d0ff3207eb25a072d9cac9825cb3
iqhsansaleh <iqhsansaleh3@gmail.com>
2026-09-18 08:57:18 +0700

    Add even or odd number checker in 01_genap_ganjil.py

---
+++ latihan-03/01_genap_ganjil.py
@@ -0,0 +1,5 @@
+bilangan = int(input("Masukkan bilangan bulat: "))  
+if bilangan % 2 == 0:
+        print(f"{bilangan} adalah bilangan genap.") 
+else:
+        print(f"{bilangan} adalah bilangan ganjil.") 
